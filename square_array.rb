1cc59c930fed97196432dba8fd297406cbb49f00
Allison Ricker <alliricker@gmail.com>
2020-09-22 17:47:36 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,7 @@
 def square_array(array)
   new_array = []
-  array.each do {|numbers| new_array << numbers**}
-    new_array
+  array.each do |numbers|
+    new_array << numbers ** 2
   end
+  return new_array
 end
